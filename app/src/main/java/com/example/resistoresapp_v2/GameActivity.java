2ad2cf4dcfde780e363d2e958f71fbb64dadc055
RemLiquit <ceixpp@gmail.com>
2022-06-14 22:53:22 -0400

    Principal Acticity, fail intent to GameActivity

---
+++ app/src/main/java/com/example/resistoresapp_v2/GameActivity.java
@@ -0,0 +1,17 @@
+package com.example.resistoresapp_v2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
--- app/src/main/java/com/example/resistoresapp_v2/PrincipalActivity.java
+++ app/src/main/java/com/example/resistoresapp_v2/PrincipalActivity.java
@@ -2,13 +2,23 @@ package com.example.resistoresapp_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PrincipalActivity extends AppCompatActivity {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(PrincipalActivity.this,GameActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
